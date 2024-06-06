ec8cce5269d8f3d0dac15a84b529c5ade579278e
dartdaven <139046041+dartdaven@users.noreply.github.com>
2024-06-06 17:22:53 +0400

    Empty Project

---
+++ Source/Agora/Agora.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Agora.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Agora, "Agora" );
+
+DEFINE_LOG_CATEGORY(LogAgora)
+ 
---
+++ Source/Agora/AgoraGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AgoraGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAgoraGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAgoraGameMode();
+};
+
+
+
